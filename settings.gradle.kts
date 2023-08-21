95a4a3d179bcf3eeeeef849e440367784d730357
taut0logy <103948698+taut0logy@users.noreply.github.com>
2023-08-21 12:41:23 +0600

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "lab003_30"
+include(":app")
+ 
